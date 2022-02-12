2aceeedd09ce1c51dcfd17669eb727ea7fe7c4af
Arvind M <mhtocs@gmail.com>
2022-02-12 16:13:08 +0530

    register meetings blueprint & use server-side session with redis

--- api/app.py
+++ api/app.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, session
 from dotenv import load_dotenv
 from config import Config
 from api.oauth import oauth_bp
+from api.meetings import meetings_bp
+from flask_session import Session
+
+import coloredlogs
 
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
+    coloredlogs.install(logger=app.logger)
+    app.url_map.strict_slashes = True
     app.config.from_object(Config)
+    Session(app)
+
     app.register_blueprint(oauth_bp)
+    app.register_blueprint(meetings_bp, url_prefix="/api/meetings")
+
+    @app.route("/no_login")
+    def no_login():
+        print(session)
+        return "You are not logged in <br><a href = '/login'>" + "click here to log in</a>"
 
     @app.route("/")
     def index():
--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 from dotenv import load_dotenv
 import os
+import redis
 
 load_dotenv()
 
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "secret")
+
+    SESSION_TYPE = "redis"
+    SESSION_PERMANANT = False
+    SESSION_USE_SIGNER = True
+    SESSION_REDIS = redis.from_url(
+        os.environ.get("REDIS_URL", "redis://127.0.0.1:6379"))
